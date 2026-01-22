cfba1921258e218471a1538f284cfab97363c098
Atomos <60550452+Atomos821@users.noreply.github.com>
2026-01-22 16:14:57 +0100

    update - Placeholders for Home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,10 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroSection } from '../../shared/ui/hero-section/hero-section';
 import { TextBlock } from '../../shared/ui/text-block/text-block';
 import { PitchBlock } from '../../shared/ui/pitch-block/pitch-block';
+import { ActionsBlock } from '../../shared/ui/actions-block/actions-block';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSection, TextBlock, PitchBlock],
+  imports: [HeroSection, TextBlock, PitchBlock, ActionsBlock],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
